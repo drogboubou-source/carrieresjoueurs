3192bcefbd890a1e71fed7eb52047200531eb75a
drogboubou-source <drogboubou@gmail.com>
2026-08-22 20:16:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'carriere-v37';
+const CACHE = 'carriere-v38';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './sync-config.js'];
 
 self.addEventListener('install', (e) => {
